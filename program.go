cf9d61ed190be1e4bed025309450de2234b84177
khalilSediq <Khalilullahsadique1334@gmail.com>
2026-06-03 14:41:41 +0500

    Refactor output statements to use fmt package for consistency

--- program.go
+++ program.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello, World!")
+	fmt.Println("Hello, World!")
+	x := make([]int, 0, 1000)
+	for i := 0; i < cap(x); i++ {
+		x = append(x, i+1)
+	}
+	y := append(x, 1001)
+	fmt.Println("The Items in X are:", x, "The Length of X is:", len(x), "The Capacity of X is:", cap(x))
+	fmt.Println("The Items in Y are:", y, "The Length of Y is:", len(y), "The Capacity of Y is:", cap(y))
 }

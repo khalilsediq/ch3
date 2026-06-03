f9662fd8595d1ebc9c23b4b1902f3d8d171bb396
khalilSediq <Khalilullahsadique1334@gmail.com>
2026-06-03 15:45:41 +0500

    Remove commented-out code and call emojeTxt function to display string length

--- program.go
+++ program.go
@@ -3,12 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-	x := make([]int, 0, 1000)
-	for i := 0; i < cap(x); i++ {
-		x = append(x, i+1)
-	}
-	y := append(x, 1001)
-	fmt.Println("The Items in X are:", x, "The Length of X is:", len(x), "The Capacity of X is:", cap(x))
-	fmt.Println("The Items in Y are:", y, "The Length of Y is:", len(y), "The Capacity of Y is:", cap(y))
+	// fmt.Println("Hello, World!")
+	// x := make([]int, 0, 1000)
+	// for i := 0; i < cap(x); i++ {
+	// 	x = append(x, i+1)
+	// }
+	// y := append(x, 1001)
+	// fmt.Println("The Items in X are:", x, "The Length of X is:", len(x), "The Capacity of X is:", cap(x))
+	// fmt.Println("The Items in Y are:", y, "The Length of Y is:", len(y), "The Capacity of Y is:", cap(y))
+	emojeTxt()
+}
+
+func emojeTxt() {
+	// var s string = "Hello 😀"
+	// var s2 string = s[6:7]
+	// var s3 string = s[:5]
+	// var s4 string = s[6:]
+	// fmt.Println(s2)
+	// fmt.Println(s3)
+	// fmt.Println(s4)
+
+	var s string = "H2"
+	fmt.Println(len(s))
 }

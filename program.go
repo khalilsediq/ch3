36186043161448b2e173d64185e6c1126f967219
khalilSediq <Khalilullahsadique1334@gmail.com>
2026-06-03 19:06:16 +0500

    Remove unused code and enhance numbers function with detailed comments

--- program.go
+++ program.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// fmt.Println("Hello, World!")
 	// x := make([]int, 0, 1000)
@@ -36,9 +34,20 @@ func numbers() {
 	// fmt.Println(x)
 	// fmt.Println(b)
 
-	s := []int{10, 20, 30}
-	s2 := make([]int, len(s))
-	copy(s2, s)
-	fmt.Println(s)
-	fmt.Println(s2)
+	// s := []int{10, 20, 30}
+	// s2 := make([]int, len(s))
+	// copy(s2, s)
+	// fmt.Println(s)
+	// fmt.Println(s2)
+
+	s1 := []int{1, 2, 3}
+	s2 := s1[0:2]             // Shared view. s2 is now [1,2]
+	s3 := append(s2, 4, 5, 6) // Appending that triggers reallocation. s3 is [1,2,4,5,6]
+	s3[0] = 99                //s3 is [99,2,4,5,6], s2[1,2,4] and it completely exhausted on it's own capacity, so s3 created it's own completely new slice and added these [99, 2,4,5,6]
+
+	s4 := make([]int, 2) // so s4 is [0,0]
+	copy(s4, s1)         // Independent copy so now s4 is [1,2]
+	s4[1] = 88           // and the value of s4's 1st indes is 88 which is [1 , 88].
+	//So in the conclusion is that s2 becomes [1,2] and then the s3 appends into s2 and the s2 still curently holds one capacity so it drops 4 into himself, which he becomes s2[1,2,4]. And s3 becomes his own independent slice, so he holds s3[1,2,4,5,6]. after that we do s3[0] = 99 which the s3 0 index value becomes 99, so now s3 is [99, 2, 4,5,6]. and finaly we come to s4 which holds 2 values of [0, 0], and then we copy s1 into it so now s4 is [1,2], and then we do s4[1] = 88, which changes the s4 to [2,88]. that's it.
+
 }
